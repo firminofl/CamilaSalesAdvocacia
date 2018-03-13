e01ad38612574c2d8f992daf9392940a7578af7a
Filipe Firmino <filipefirmino@gec.inatel.br>
2018-03-12 22:17:49 -0300

    Cadastro no caixa.

---
+++ app/src/main/java/camilasales/camilasalesadvocacia/Caixa_Cadastra_Activity.java
@@ -0,0 +1,13 @@
+package camilasales.camilasalesadvocacia;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Caixa_Cadastra_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_caixa__cadastra_);
+    }
+}
