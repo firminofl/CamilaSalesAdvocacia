659802969548303abde7a63f9b42a86a971c4d7c
Filipe Firmino <filipefirmino@gec.inatel.br>
2018-04-01 16:45:58 -0300

    Conexão com banco de dados efetuado e tela sobre atualizada
    Signed-off-by: Filipe Firmino <filipefirmino@gec.inatel.br>

---
+++ app/src/main/java/camilasales/camilasalesadvocacia/control/activity/Caixa_Cadastra_Activity.java
@@ -0,0 +1,15 @@
+package camilasales.camilasalesadvocacia.control.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import camilasales.camilasalesadvocacia.R;
+
+public class Caixa_Cadastra_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_caixa__cadastra_);
+    }
+}
--- app/src/main/java/camilasales/camilasalesadvocacia/fragments/FisicaHomeFragment.java
+++ app/src/main/java/camilasales/camilasalesadvocacia/fragments/FisicaHomeFragment.java
@@ -9,7 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-import camilasales.camilasalesadvocacia.control.CadastroEditarActivity;
+import camilasales.camilasalesadvocacia.control.activity.CadastroEditarActivity;
 import camilasales.camilasalesadvocacia.R;
 
 /**
