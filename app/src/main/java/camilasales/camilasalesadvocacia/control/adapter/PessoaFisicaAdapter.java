ad9ca8b6e166b4295b6e5afcc1deb6cef89db244
Filipe Firmino <filipefirmino@gec.inatel.br>
2018-05-08 21:54:49 -0300

    Crud pessoa fisica em andamento de projeto
    Signed-off-by: Filipe Firmino <filipefirmino@gec.inatel.br>

--- app/src/main/java/camilasales/camilasalesadvocacia/control/adapter/PessoaFisicaAdapter.java
+++ app/src/main/java/camilasales/camilasalesadvocacia/control/adapter/PessoaFisicaAdapter.java
@@ -1,4 +1,38 @@
 package camilasales.camilasalesadvocacia.control.adapter;
 
-public class PessoaFisicaAdapter {
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+
+import camilasales.camilasalesadvocacia.R;
+import camilasales.camilasalesadvocacia.model.Entidades.PessoaFisica;
+
+public class PessoaFisicaAdapter extends ArrayAdapter<PessoaFisica> {
+
+    private ArrayList<PessoaFisica> pessoaFísica;
+    private Context context;
+
+    public PessoaFisicaAdapter(Context c,ArrayList<PessoaFisica> objectsPF) {
+        super(c,0 , objectsPF);
+        this.pessoaFísica = objectsPF;
+        this.context = c;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        View view = null;
+
+        if(pessoaFísica != null){
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(R.layout.fragment_fisica, parent, true);
+        }
+        return view;
+    }
 }
--- app/src/main/java/camilasales/camilasalesadvocacia/model/Entidades/PessoaFisica.java
+++ app/src/main/java/camilasales/camilasalesadvocacia/model/Entidades/PessoaFisica.java
@@ -1,4 +1,134 @@
 package camilasales.camilasalesadvocacia.model.Entidades;
 
 public class PessoaFisica {
+
+    private String nome;
+    private int cpf;
+    private int rg;
+    private int registro_cnh;
+    private String sexo;
+    private String data_nasc;
+    private String endereco;
+    private int numero;
+    private String cidade;
+    private String estado;
+    private String bairro;
+    private int cep;
+    private String email;
+    private String profissao;
+
+    public PessoaFisica() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+
+    public int getRg() {
+        return rg;
+    }
+
+    public void setRg(int rg) {
+        this.rg = rg;
+    }
+
+    public int getRegistro_cnh() {
+        return registro_cnh;
+    }
+
+    public void setRegistro_cnh(int registro_cnh) {
+        this.registro_cnh = registro_cnh;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getData_nasc() {
+        return data_nasc;
+    }
+
+    public void setData_nasc(String data_nasc) {
+        this.data_nasc = data_nasc;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public int getCep() {
+        return cep;
+    }
+
+    public void setCep(int cep) {
+        this.cep = cep;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getProfissao() {
+        return profissao;
+    }
+
+    public void setProfissao(String profissao) {
+        this.profissao = profissao;
+    }
 }
