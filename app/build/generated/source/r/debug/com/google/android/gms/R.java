7b2a0536b0563219b02ca4e4d4a65038006a59e5
Kaline Dias <kalinedias@gec.inatel.br>
2018-06-11 08:09:17 -0300

    Tela de cadastro de processo concluído
    Signed-off-by: Kaline Dias <kalinedias@gec.inatel.br>

--- app/build/generated/source/r/debug/com/google/android/gms/R.java
+++ app/build/generated/source/r/debug/com/google/android/gms/R.java
@@ -57,17 +57,17 @@ public final class R {
         public static final int auto = 0x7f090024;
         public static final int center = 0x7f090030;
         public static final int dark = 0x7f09003e;
-        public static final int icon_only = 0x7f090084;
-        public static final int light = 0x7f090094;
-        public static final int none = 0x7f0900b3;
-        public static final int normal = 0x7f0900b4;
-        public static final int radio = 0x7f0900c0;
-        public static final int standard = 0x7f0900ee;
-        public static final int text = 0x7f0900f5;
-        public static final int text2 = 0x7f0900f6;
-        public static final int toolbar = 0x7f090101;
-        public static final int wide = 0x7f090128;
-        public static final int wrap_content = 0x7f09012b;
+        public static final int icon_only = 0x7f090089;
+        public static final int light = 0x7f09009f;
+        public static final int none = 0x7f0900c0;
+        public static final int normal = 0x7f0900c1;
+        public static final int radio = 0x7f0900cd;
+        public static final int standard = 0x7f0900ff;
+        public static final int text = 0x7f090106;
+        public static final int text2 = 0x7f090107;
+        public static final int toolbar = 0x7f090112;
+        public static final int wide = 0x7f090139;
+        public static final int wrap_content = 0x7f09013c;
     }
     public static final class integer {
         public static final int google_play_services_version = 0x7f0a0007;
